fac7d55d391a29b0c00e1554bd6b6e6ee53efc48
Byunggyu Park <55678893+ggyuchive@users.noreply.github.com>
2022-04-06 22:23:30 +0900

    Create DFS.cpp

---
+++ Base_Codes/Graph/DFS.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> edge;
+vector<bool> visited;
+int n, m;
+int ans_DFS = 0;
+
+void DFS(int s) {
+    visited[s] = true;
+    for (int i = 0; i < edge[s].size(); i++) {
+        int e = edge[s][i];
+        if (!visited[e]) DFS(e);
+    }
+}
+
+void DFS_all() {
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {DFS(i); ans_DFS++;}
+    }
+}
+
+int main() {
+    ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> n >> m;
+    edge.resize(n, vector<int>());
+    int a,b;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b; a--; b--;
+        edge[a].push_back(b);
+        edge[b].push_back(a);
+    }
+    visited.resize(n,false);
+    DFS_all();
+    cout << ans_DFS;
+}
